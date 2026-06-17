7078123da7fca9bf8bde7f5c364f9e2c21928603
Paulo Vitor <Teodoropaulo1504@gmail.com>
2026-06-16 21:23:09 -0300

    Preparação das Rotas e Template do Controle de Noticias

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,17 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="assets/img/logo.svg" alt="Portal Notícias">
+                        <img src="{{ asset('assets/img/logo.svg') }}" alt="Portal Notícias">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                       Home
+                    </x-nav-link>
+                    <x-nav-link :href="route('admin.noticias.index')" :active="request()->routeIs('admin.noticias.index')">
+                       Notícias
                     </x-nav-link>
                 </div>
             </div>
